6fb8342c2db22f17292c6e4163068f091605839c
JianjiC <jianjichen001@gmail.com>
2025-05-29 12:32:41 +0200

    Update tarea_s4_JianjiC.sql

--- tarea_s4_JianjiC.sql
+++ tarea_s4_JianjiC.sql
@@ -13,7 +13,7 @@ CREATE TABLE companies(
 	website VARCHAR(255));
     
 CREATE TABLE credit_cards(
-    id VARCHAR(20) PRIMARY KEY,
+    card_id VARCHAR(20) PRIMARY KEY,
     user_id INT REFERENCES users(id),
     iban VARCHAR(50),
     pan VARCHAR(50),
@@ -24,7 +24,7 @@ CREATE TABLE credit_cards(
     expiring_date VARCHAR(20));
 
 CREATE TABLE users(
-    id INT PRIMARY KEY,
+    user_id INT PRIMARY KEY,
     name VARCHAR(100),
     surname VARCHAR(100),
     phone VARCHAR(150),
@@ -36,9 +36,9 @@ CREATE TABLE users(
     address VARCHAR(255));
     
 CREATE TABLE transactions(
-    id VARCHAR(255) PRIMARY KEY,
+    transaction_id VARCHAR(255) PRIMARY KEY,
     card_id VARCHAR(15) REFERENCES credit_cards(id),
-    business_id VARCHAR(20) REFERENCES companies(company_id),
+    company_id VARCHAR(20) REFERENCES companies(company_id),
     timestamp TIMESTAMP,
     amount DECIMAL(10, 2),
     declined BOOLEAN,
@@ -46,9 +46,9 @@ CREATE TABLE transactions(
     user_id INT REFERENCES users(id),
     lat FLOAT,
     longitude FLOAT,
-    FOREIGN KEY (business_id) REFERENCES companies(company_id),
-    FOREIGN KEY (card_id) REFERENCES credit_cards(id),
-    FOREIGN KEY (user_id) REFERENCES users(id));
+    FOREIGN KEY (company_id) REFERENCES companies(company_id),
+    FOREIGN KEY (card_id) REFERENCES credit_cards(card_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id));
 
 LOAD DATA LOCAL INFILE
 "C:\\EEE\\IT Academy_Analisis de Dades\\Especialitazacio_DA\\Tasca S4.01. Creacio de Base de Dades\\companies.csv"
@@ -109,9 +109,9 @@ IGNORE 1 LINES;
 SELECT u.*
 FROM users u
 INNER JOIN transactions t
-ON u.id = t.user_id
-GROUP BY u.id
-HAVING COUNT(t.id) > 30;
+ON u.user_id = t.user_id
+GROUP BY u.user_id
+HAVING COUNT(t.transaction_id) > 30;
 
 ### Exercici 2
 # Muestra la media de amount por IBAN de las tarjetas de crédito en la compañía Donec Ltd.,
@@ -119,9 +119,9 @@ HAVING COUNT(t.id) > 30;
 SELECT c.company_name, cc.iban, AVG(t.amount) AS mean_sale
 FROM transactions t
 INNER JOIN credit_cards cc
-ON t.card_id = cc.id
+ON t.card_id = cc.card_id
 INNER JOIN companies c
-ON t.business_id = c.company_id
+ON t.company_id = c.company_id
 WHERE c.company_name = "Donec Ltd"
 GROUP BY c.company_name, cc.iban;
 
@@ -132,18 +132,24 @@ GROUP BY c.company_name, cc.iban;
 ### Exercici 1 Quantes targetes estan actives?
 ## (1) crear la tabla
 CREATE TABLE card_status
-SELECT tt.card_id,
+SELECT 
+    CONCAT(tt.transaction_id, "_", tt.card_id) AS transaction_card_id,
+    tt.transaction_id, tt.card_id,
     STR_TO_DATE(cc.expiring_date, '%m/%d/%Y') AS expiring_date,
-    tt.id AS transaction_id,
     tt.timestamp, tt.declined
 FROM credit_cards cc
 JOIN (
     SELECT *,
         ROW_NUMBER() OVER (PARTITION BY card_id ORDER BY timestamp DESC) AS rn
     FROM transactions) tt
-ON cc.id = tt.card_id
+ON cc.card_id = tt.card_id
 WHERE tt.rn <= 3;
+
+ALTER TABLE card_status
+	MODIFY transaction_card_id VARCHAR(255) PRIMARY KEY,
+    ADD FOREIGN KEY (card_id) REFERENCES credit_cards(card_id);    
 SELECT * FROM card_status;
+
 ## (2) consulta: cantidad de tarjectas activas actucalmente
 SELECT COUNT(DISTINCT card_id) AS num_card_active
 FROM card_status
@@ -157,7 +163,7 @@ WHERE expiring_date > NOW();
 ### Exercici 1: Necessitem conèixer el nombre de vegades que s'ha venut cada producte.
 ## (1) crear la tabla "products" e introducir los datos
 CREATE TABLE products(
-    id INT PRIMARY KEY,
+    product_id INT PRIMARY KEY,
     product_name VARCHAR(100),
     price VARCHAR(20),
     colour VARCHAR(20),
@@ -175,7 +181,7 @@ IGNORE 1 LINES;
 
 UPDATE products
 SET price = REPLACE(price, "$", "")
-WHERE id <> "";
+WHERE product_id <> "";
 
 ALTER TABLE products
 MODIFY price DECIMAL(10, 2);
@@ -185,29 +191,33 @@ SELECT * FROM products;
 ## (2) encontrar la cantidad maxima de productos por transaction
 SELECT SUM(LENGTH(product_ids) - LENGTH(REPLACE(product_ids, ",", "")) + 1) AS num_product
 FROM transactions
-GROUP BY id
+GROUP BY transaction_id
 ORDER BY num_product DESC
 LIMIT 1;
 
-## (3) crear una tabla “numbers” de numeros de 1 hasta 4, la cantidad maxima de productos por transaction
+## (3) crear una tabla “numbers” de numeros de 1 hasta 10 (ser flexible), más que la cantidad maxima de productos por transaction
 ## para separar product_ids en la tabla transactions
 CREATE TABLE numbers(
   n int);
-INSERT INTO numbers VALUES (1),(2),(3),(4);
+INSERT INTO numbers VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10);
 
 ## (4) separar product_ids y crear una nueva tabla
 CREATE TABLE transactions_products
 SELECT
-	id AS transaction_id,
-    SUBSTRING_INDEX(SUBSTRING_INDEX(t.product_ids, ", ", n.n), ", ", -1) product_id
+    CONCAT(t.transaction_id, "_",
+        SUBSTRING_INDEX(SUBSTRING_INDEX(t.product_ids, ", ", n.n), ", ", -1))
+		AS transaction_product_id,
+	transaction_id,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(t.product_ids, ", ", n.n), ", ", -1) AS product_id
 FROM transactions t
 INNER JOIN numbers n
 ON n.n <= LENGTH(t.product_ids) - LENGTH(REPLACE(t.product_ids, ",", "")) + 1;
 
 ALTER TABLE transactions_products
+    ADD PRIMARY KEY(transaction_product_id),
+    ADD FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id),
     MODIFY product_id INT,
-    ADD FOREIGN KEY (transaction_id) REFERENCES transactions(id),
-    ADD FOREIGN KEY (product_id) REFERENCES products(id);
+    ADD FOREIGN KEY (product_id) REFERENCES products(product_id);
 
 SELECT * FROM transactions_products;
 
@@ -215,9 +225,9 @@ SELECT * FROM transactions_products;
 SELECT tp.product_id, p.product_name, COUNT(DISTINCT tp.transaction_id) AS num_transactions
 FROM transactions t
 JOIN transactions_products tp
-ON t.id = tp.product_id
+ON t.transaction_id = tp.transaction_id
 JOIN products p
-ON tp.product_id = p.id
+ON tp.product_id = p.product_id
 WHERE t.declined = 0
 GROUP BY tp.product_id
 ORDER BY num_transactions DESC;
